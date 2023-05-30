a55783299bdc24c9a9b3c85ac7ae538bce42e9df
jayson <jaysinscars@gmail.com>
2023-05-30 09:12:16 +0100

    feat: added authentication and functionality

--- src/base/middleware.py
+++ src/base/middleware.py
@@ -320,7 +320,7 @@ class RequestResponseMiddleware(BaseHTTPMiddleware):
             serializer = resource.serializers.get("%s_response" % obj_id, serializer)
         if resource_name:
             serializer = resource.serializers.get("%s_response" % resource_name, serializer)
-        if not getattr(response, "context"):
+        if not getattr(response, "context", None):
             return response
 
         context = response.context
